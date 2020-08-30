52960688c18b253044b4ab5fee41ac892dd4051e
Teatek <teatek0512@gmail.com>
2020-08-30 12:21:35 +0200

    New button + modal window

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,24 @@ window.onload = function setTime() {
     let date = new Date;
     time.outerHTML = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
 }
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+document.getElementById("new-card").addEventListener("click", () => {
+    modal.style.display = "block";
+});
